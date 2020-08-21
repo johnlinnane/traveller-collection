7c063ae058f5414d63b4b5fd99cea7ff37dfab53
Panister Fatheroy <panisterfatheroy@gmail.com>
2020-08-21 14:43:30 +0100

    tidy coll list

--- client/src/components/Collections/list.js
+++ client/src/components/Collections/list.js
@@ -28,67 +28,17 @@ class CollList extends Component {
             type: 'Collections'
         }
     }
-    // componentWillUnmount() {
-    //     this.props.dispatch(clearItemWithContributor());
-    // }
-
-
-
-
-    // renderItem = (items) => {
-    //     console.log(items);
-    //     return ( items.item ?
-    //         <div className="br_container">
-    //             <div className="br_header">
-    //                 <h2>{items.item.title}</h2>
-    //                 <h5>{items.item.creator}</h5>
-    //                 <div className="br_reviewer">
-    //                     {/* <span>Reviewed by: </span>{items.reviewer.name} {items.reviewer.lastname} */}
-    //                     <span>Reviewed by: </span>{items.contributor.name} {items.contributor.lastname}
-    //                 </div>
-    //             </div>
-
-    //             <div className="br_review">
-    //                 {items.item.description}
-    //             </div>
-
-    //             <div className="br_box">
-                    
-    //                 <div className="left">
-    //                     <div>
-    //                         <span>Pages:</span> {items.item.pages}
-    //                     </div>
-    //                     <div>
-    //                         <span>ID:</span> {items.item.id}
-    //                     </div>
-    //                 </div>
-                        
-    //                 <div className="right">
-    //                     <span>ID</span>
-    //                     <div>{items.item.id}</div>
-    //                 </div>
-    //             </div>
-
-
-    //         </div>
-    //     : null
-    // )}
 
 
     render() {
-        
-        // console.log(this.props.collections);
-        
-        // console.log(this.props.match.params.id);
 
 
         let collections = this.props.collections;
 
         let items = null;
 
-        if (this.props.collections) {
+        if (this.props.collections && this.props.collections.length) {
             items = collections.map( (item, i) => {
-                // console.log(item);
                 return (
                     <Link key={item._id} to={`/collection/${item.id}`}>
                         <CollListItem item={item}/>
@@ -105,12 +55,7 @@ class CollList extends Component {
 
                 <div className="main_view">
                     <div>
-                        {/* {this.props.collections ? */}
-                        
-                            
-                                {items}
-                                
-                        {/* : null} */}
+                            {items}
                     </div>
                 </div>
             </div>
