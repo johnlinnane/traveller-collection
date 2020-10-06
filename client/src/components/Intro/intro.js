2a370e18b0968c45324e7cf7a78088e85d0ad526
Panister Fatheroy <panisterfatheroy@gmail.com>
2020-10-06 18:25:22 +0100

    intro

--- client/src/components/Intro/intro.js
+++ client/src/components/Intro/intro.js
@@ -28,43 +28,35 @@ class Intro extends Component  {
     render() {
         console.log(process.env.NODE_ENV);
         return (
-            // <Link to="/categories">
 
+                <div className="intro">
+                    
+                    <div id="intro-main">
+                        <div className="bg-image"></div>
+                        <div className="content-wrap">
+                            {this.props.text && this.props.text.title ?
+                            <h1>{this.props.text.title}</h1>
+                            : null }
+                            {this.props.text && this.props.text.body ?
+                            <p>{this.props.text.body}</p>
 
+                            
 
-                // <header id="showcase" class="grid">
-                //     <div class="bg-image"></div>
-                //     <div class="content-wrap">
-                //     <h1>Welcome to Acme Web Solutions</h1>
-                //     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci eum error earum soluta voluptatum nisi
-                //         laboriosam eos saepe asperiores dolorum.</p>
-                //     <a href="#section-b" class="btn">Read More</a>
-                //     </div>
-                // </header>
-
-
-                <div id="intro">
-                    <div className="bg-image"></div>
-                    <div className="content-wrap">
-                        {this.props.text && this.props.text.title ?
-                        <h1>{this.props.text.title}</h1>
-                        : null }
-                        {this.props.text && this.props.text.body ?
-                        <p>{this.props.text.body}</p>
-
+                            : null}
+                            <a href="/categories" className="btn">Browse</a>
+                            
+                        </div>
                         
 
-                        : null}
-                        <a href="/categories" className="btn">Enter</a>
                         
+                            {/* <img src={`/media/intro/intro.jpg`} alt={"Item"} onError={addDefaultImg}/> */}
+                    
                     </div>
-                    
-
-                    
-                        {/* <img src={`/media/intro/intro.jpg`} alt={"Item"} onError={addDefaultImg}/> */}
+                    <div className="intro-items">
+                        <p>Hello</p>
+                        </div>
                 
                 </div>
-            // </Link>
         );
     }
 };
