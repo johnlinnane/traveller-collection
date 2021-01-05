e15dcd4b69f2bff64d4ad302866876d59339c890
Panister Fatheroy <panisterfatheroy@gmail.com>
2021-01-05 00:58:29 +0000

    routing hocs

--- client/src/components/widgetsUI/Slider/slider.js
+++ client/src/components/widgetsUI/Slider/slider.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import SliderTemplates from './slider_templates'
-import { getItems } from '../../../../actions';
+import { getItems } from './../../../actions';
 
 
 class NewsSlider extends Component {
