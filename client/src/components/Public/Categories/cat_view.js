e01a591541e35e0833359cb9231e32a996a23c24
Panister Fatheroy <panisterfatheroy@gmail.com>
2021-01-05 00:53:35 +0000

    routing hocs

--- client/src/components/Public/Categories/cat_view.js
+++ client/src/components/Public/Categories/cat_view.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { getItemsByCat, getCatById, getAllSubCats } from '../../../actions';
-import NavigationBar from '../widgetsUI/navigation';
+import NavigationBar from '../../widgetsUI/navigation';
 
 
 class CatView  extends Component {
