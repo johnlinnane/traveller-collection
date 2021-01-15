2bd2fd1b73155a24df2fa256eeb1fb32a7c4b10e
John Linnane <41014182+johnlinnane@users.noreply.github.com>
2021-01-15 21:19:38 +0000

    Delete client directory

--- client/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const config = require('./config_client').get(process.env.NODE_ENV);
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-    
-module.exports = function(app) {
-    app.use(createProxyMiddleware("/api/", { target: `http://${config.IP_ADDRESS}:3001/` }))
-}
