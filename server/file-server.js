e51ed4989c5ecf73e4d788500a0e5a6264e8e704
Panister Fatheroy <panisterfatheroy@gmail.com>
2021-03-11 11:40:28 +0000

    dev branch test

--- server/file-server.js
+++ server/file-server.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 
 app.use(express.static('public'));
-console.log("FILE-SERVER running on port 4000");
+console.log("FILE-SERVER: running on port 4000");
 
 
 server.listen(4000);
