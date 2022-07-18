a1fa141cb1a2fa3bafb84aebe7c2ff5804af2223
John Linnane <johnlinnane123@gmail.com>
2022-07-18 13:26:23 +0100

    header.js: class => func

--- client/src/components/Public/Header/header.js
+++ client/src/components/Public/Header/header.js
@@ -1,66 +1,37 @@
-import React, { Component } from 'react';
-
+import React, { useState } from 'react';
 import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router-dom';
 import Nav from './Sidenav/sidenav';
 
-class Header extends Component {
+export default function Header() {
+    const [showNav, setShowNav] = useState(false);
 
-    state = {
-        showNav:false
-    }
-
-
-    onHideNav = () => {
-        this.setState({showNav:false})
-    }
-
-    render() {
-        return (
-            <header>
-                <div className="open_nav">
-                    <FontAwesome 
-                        className="bars"
-                        name="bars" 
-                        onClick={() => this.setState({showNav:true})}
-                    />
-                </div>
-
-                <Nav
-                    showNav={this.state.showNav}
-                    // trigger a function whenever button clicked
-                    onHideNav={() => this.onHideNav()}
-                    className="sidenav_menu"
-
+    return (
+        <header>
+            <div className="open_nav">
+                <FontAwesome 
+                    className="bars"
+                    name="bars" 
+                    onClick={() => setShowNav(true)}
                 />
-                <div className="search">
-                    <Link to="/search">
+            </div>
+            <Nav
+                showNav={showNav}
+                onHideNav={() => setShowNav(false)}
+                className="sidenav_menu"
 
-                        <FontAwesome 
-                            name="search" 
-                            className="bars"
-                            // onClick={() => this.setState({showNav:true})}
-                         
-                        />
-                    </Link>
-                </div>
-                
-                <Link to="/" className="logo">
-                    Traveller Collection
+            />
+            <div className="search">
+                <Link to="/search">
+                    <FontAwesome 
+                        name="search" 
+                        className="bars"
+                    />
                 </Link>
-
-                
-
-                
-
-
-                
-
-
-               
-            </header>
-        );
-    }
+            </div>
+            <Link to="/" className="logo">
+                Traveller Collection
+            </Link>
+        </header>
+    );
 }
-
-export default Header;
