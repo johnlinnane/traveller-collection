cad07605ae1dda07092029aebf6fc04aa15a4154
John Linnane <johnlinnane123@gmail.com>
2022-09-13 18:45:41 +0100

    fixed typos in disclaimer

--- client/src/components/Public/Intro/intro.js
+++ client/src/components/Public/Intro/intro.js
@@ -56,7 +56,7 @@ const Intro = props =>  {
                     background: '#aeddeb'
                 }}
             >
-                This collection includes descriptions and links to descriptions and representations of Travellers that may be factually inaccurate, offensive and otherwise inappropriate. Part of the work of compiling this collection involves adressing discrimatory legacies and we ask for your help in identifiying images or data that cause offence or harm, your feedback and suggestions are very welcome and can be sent to info@TravellerCollection.ie.
+                This collection includes descriptions and links to descriptions and representations of Travellers that may be factually inaccurate, offensive and otherwise inappropriate. Part of the work of compiling this collection involves addressing discriminatory legacies and we ask for your help in identifying images or data that cause offence or harm, your feedback and suggestions are very welcome and can be sent to info@TravellerCollection.ie. 
             </div>
         </div>
     );
