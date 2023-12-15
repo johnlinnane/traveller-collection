1775e313de0284c635c342625b47880eb857962c
johnlinnane <johnlinnane123@gmail.com>
2023-12-15 14:17:53 +0000

    [sligo-map] added background image

--- client/src/components/Public/SligoMap/index.tsx
+++ client/src/components/Public/SligoMap/index.tsx
@@ -4,13 +4,38 @@ import { connect } from 'react-redux';
 const SligoMap = (props: any) => {
 
     return (
-        <div>
+        <div
+            style={{
+                position: 'relative', // Set container position to relative
+                width: '100vw', // 100% of viewport width
+                height: '100vh', // 100% of viewport height
+                margin: 0,
+                padding: 0,
+                overflow: 'hidden'
+            }}
+        >
+            <img
+                alt="MapUpdated"
+                src="/assets/media/sligo-map/MapUpdated.png"
+                style={{ 
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover', 
+                    zIndex: -1 
+                }}
+            />
             <img 
                 alt="Leafy Frame"
                 src="/assets/media/sligo-map/Frame-Full.png"
                 style={{
-                    width: '100vw',
-                    height: '100vh',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
                     objectFit: 'cover'
                 }}
             />
