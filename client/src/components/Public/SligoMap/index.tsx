94f96bfeb90f0cfa11359ff4b24823f9c988bbd0
johnlinnane <johnlinnane123@gmail.com>
2024-02-22 21:14:45 +0000

    sligo: added a real map

--- client/src/components/Public/SligoMap/index.tsx
+++ client/src/components/Public/SligoMap/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+// import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from 'leaflet'
+
 const SligoMap = (props: any) => {
+
+    var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
 
     return (
         <div
@@ -14,7 +25,7 @@ const SligoMap = (props: any) => {
                 overflow: 'hidden'
             }}
         >
-            <img
+            {/* <img
                 alt="MapUpdated"
                 src="/assets/media/sligo-map/MapUpdated.png"
                 style={{ 
@@ -26,7 +37,47 @@ const SligoMap = (props: any) => {
                     objectFit: 'cover', 
                     zIndex: -1 
                 }}
-            />
+            /> */}
+
+<MapContainer 
+                className="main_map"
+                center={[54.20638315779152, -8.57869767149748]} 
+                zoom={9} 
+            >
+                <TileLayer
+                    attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                
+
+                {/* {props.items.items && props.items.items.length ?
+                    <div>
+                        {props.items.items.map( (item, i) => (
+                            item.geo && item.geo.latitude && item.geo.longitude ?
+                                <Marker 
+                                    position={[item.geo.latitude, item.geo.longitude]} 
+                                    key={i}
+                                    icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}
+                                >
+                                    <Popup>
+                                        <Link to={`/items/${item._id}`} target="_blank">
+                                            <span><b>{item.title}</b></span>
+                                            <br/>
+                                            {item.geo.address ?
+                                                <div>
+                                                    <span>{item.geo.address}</span><br/>
+                                                    <br/>
+                                                </div>
+                                            : null}
+                                        </Link>
+                                    </Popup>
+                                </Marker>
+                            : null
+                        ) )}     
+                    </div>
+                : null} */}
+            </MapContainer>
+
             <img 
                 alt="Leafy Frame"
                 src="/assets/media/sligo-map/Frame-Full.png"
